314107c69029cf3ef6bab3deca7416148db650a5
Michael230398 <majkl.malik@seznam.cz>
2021-05-03 10:36:39 +0200

    add fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,22 @@
+def recursive_ntho_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        output = recursive_ntho_fibo(n-1) + recursive_ntho_fibo(n-2)
+
+        return output
+
+
 
 def main():
-    pass
+   number = int(input("Zadejte číslo:"))
+   while number < 1:
+       print("Zadali jste neplatné číslo.")
+       number = int(input("Zadejte číslo: "))
+   else:
+       return print(recursive_ntho_fibo(number - 1))
 
 
 if __name__ == '__main__':
